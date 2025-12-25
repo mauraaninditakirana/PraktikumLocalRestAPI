4616afbc911378f47d38db26492a9b1ed586c8da
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 18:18:46 +0700

    tambah repository DataSiswa dan implementasi API

--- app/src/main/java/com/example/praktikumlocalrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/praktikumlocalrestapi/repositori/RepositoryDataSiswa.kt
@@ -2,18 +2,23 @@ package com.example.praktikumlocalrestapi.repositori
 
 import com.example.praktikumlocalrestapi.apiservice.ServiceApiSiswa
 import com.example.praktikumlocalrestapi.modeldata.DataSiswa
-
-interface RepositoryDataSiswa{
+interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-    //suspend fun getStatusSiswa(id:Int) : DataSiswa
-    //suspend fun editStatusSiswa(id:Int,dataSiswa: DataSiswa) :retrofitr.Response<Void)
-    //suspend fun hapusStatusSiswa(id:Int) :retrofitr.Response<Void)
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
+
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ):RepositoryDataSiswa{
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> =
+        serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void> = serviceApiSiswa
+        .hapusSatuSiswa(id)
 }
